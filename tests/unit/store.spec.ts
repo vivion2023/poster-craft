2a2d25335528b45258ba271a288241fc7094e464
vivion <vivonhyt@gmail.com>
2025-05-16 15:59:06 +0800

    测试store用户的登入登出和template的默认值设置

--- tests/unit/store.spec.ts
+++ tests/unit/store.spec.ts
@@ -1,4 +1,5 @@
 import { testComponents, ComponentData } from "@/store/editor";
+import { testData } from "@/store/templates";
 import { TextComponentProps } from "@/defaultProps";
 import { last, clone } from "lodash-es";
 import store from "@/store";
@@ -8,5 +9,26 @@ describe("test vuex store", () => {
     expect(store.state).toHaveProperty("user");
     expect(store.state).toHaveProperty("templates");
     expect(store.state).toHaveProperty("editor");
+  });
+
+  describe("test user module", () => {
+    it("test login mutation", () => {
+      store.commit("login");
+      expect(store.state.user.isLogin).toBeTruthy();
+    });
+    it("test logout mutation", () => {
+      store.commit("logout");
+      expect(store.state.user.isLogin).toBeFalsy();
+    });
+  });
+
+  describe("test templates module", () => {
+    it("should have default templates", () => {
+      expect(store.state.templates.data).toHaveLength(testData.length);
+    });
+    it("should get the correct template by Id", () => {
+      const selectTemplate = store.getters.getTemplateById(1);
+      expect(selectTemplate.title).toBe("前端架构师海报");
+    });
   });
 });
