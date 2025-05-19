09bab1efe875c6d1b770a91e5e415b5327374f1c
vivion <vivonhyt@gmail.com>
2025-05-19 14:43:27 +0800

    编写upload组件

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,18 @@
 <template>
   <div class="app-container">
-    <form
-      method="post"
-      action="http://local.test:7001/api/upload"
-      @change="handleFileChange"
-    >
-      <input type="file" name="file" />
-      <input type="text" name="test" />
-      <input type="submit" value="提交" />
-    </form>
+    <Uploader
+      action="https://run.mocky.io/v3/dff682b1-f1f6-4ec1-ab36-c735534a6583"
+    />
     <router-view />
   </div>
 </template>
 
 <script lang="ts">
-import axios from "axios";
+import Uploader from "./components/Uploader.vue";
 export default {
   name: "App",
-  setup() {
-    const handleFileChange = (e: Event) => {
-      const target = e.target as HTMLInputElement;
-      const files = target.files;
-      if (files) {
-        const uploadedFile = files[0];
-        const formData = new FormData();
-        formData.append(uploadedFile.name, uploadedFile);
-        axios
-          .post("http://local.test:7001/api/upload", formData, {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          })
-          .then((res) => {
-            console.log(res);
-          });
-      }
-    };
-    return {
-      handleFileChange,
-    };
+  components: {
+    Uploader,
   },
 };
 </script>
---
+++ src/components/Uploader.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="file-upload">
+    <button @click="triggerUpload">
+      <span v-if="fileStatus === 'loading'">正在上传</span>
+      <span v-else-if="fileStatus === 'success'">上传成功</span>
+      <span v-else-if="fileStatus === 'error'">上传失败</span>
+      <span v-else>点击上传</span>
+    </button>
+    <input type="file" ref="fileInput" :style="{ display: 'none' }" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import axios from "axios";
+type UploadStatus = "ready" | "loading" | "success" | "error";
+export default defineComponent({
+  props: {
+    action: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const fileInput = ref<HTMLInputElement | null>(null);
+    const fileStatus = ref<UploadStatus>("ready");
+    const triggerUpload = () => {
+      if (fileInput.value) {
+        fileInput.value.click();
+      }
+    };
+
+    const handleFileChange = (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      const files = target.files;
+      if (files) {
+        const uploadedFile = files[0];
+        const formData = new FormData();
+        formData.append(uploadedFile.name, uploadedFile);
+        axios
+          .post(props.action, formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((resp) => {
+            console.log(resp.data);
+            fileStatus.value = "success";
+          })
+          .catch((err) => {
+            console.log(err);
+            fileStatus.value = "error";
+          });
+      }
+    };
+    return {
+      fileInput,
+      triggerUpload,
+      fileStatus,
+      handleFileChange,
+    };
+  },
+});
+</script>
