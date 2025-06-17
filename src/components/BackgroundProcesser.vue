58b6ff301fe8e06182ba5eb2871c234317f830b5
vivion <vivonhyt@gmail.com>
2025-06-17 23:26:10 +0800

    新增BackgroundProcesser和ShadowPicker组件，支持图像上传和阴影属性设置功能

---
+++ src/components/BackgroundProcesser.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="background-processer">
+    <styled-uploader v-if="!value" @success="onImageUploaded">
+    </styled-uploader>
+    <image-processer
+      v-else
+      :value="value"
+      @change="handleUploadUrl"
+      :showDelete="true"
+    >
+    </image-processer>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { message } from "ant-design-vue";
+import ImageProcesser from "./ImageProcesser.vue";
+import StyledUploader from "./StyledUploader.vue";
+export default defineComponent({
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    ImageProcesser,
+    StyledUploader,
+  },
+  emits: ["change"],
+  setup(props, { emit }) {
+    const onImageUploaded = (data: {
+      resp: { errno: number; data: { urls: string[] } };
+      file: File;
+    }) => {
+      const { resp } = data;
+      message.success("上传成功");
+      emit("change", resp.data.urls[0]);
+    };
+    const handleUploadUrl = (url: string) => {
+      emit("change", url);
+    };
+    return {
+      onImageUploaded,
+      handleUploadUrl,
+    };
+  },
+});
+</script>
+
+<style></style>
