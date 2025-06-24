f5ea21f00dc9887de935526e4333cea4c360c607
vivion <vivonhyt@gmail.com>
2025-06-24 15:23:11 +0800

    新增jsonwebtoken依赖，更新mock服务器以支持用户登录并生成JWT

--- fake-server/mockServer.js
+++ fake-server/mockServer.js
@@ -1,15 +1,28 @@
 // @ts-nocheck
 const jsonServer = require("json-server");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 const server = jsonServer.create();
 const router = jsonServer.router(__dirname + "/db.json");
 const middlewares = jsonServer.defaults();
+const SECRET_KEY = "1234567890"; //密钥
+const expiresIn = "1h"; //过期时间
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+}
+
 const rewriter = jsonServer.rewriter({
   "/api/*": "/$1",
 });
+server.use(bodyParser.json());
 server.use(rewriter);
 server.use(middlewares);
-server.get("/echo", (req, res) => {
-  res.jsonp({ test: "123" });
+server.post("/users/loginByPhoneNumber", (req, res) => {
+  const { phoneNumber, veriCode } = req.body;
+  const accessToken = createToken({ phoneNumber, veriCode });
+  res.status(200).jsonp({
+    token: accessToken,
+  });
 });
 router.render = (req, res) => {
   res.jsonp({
