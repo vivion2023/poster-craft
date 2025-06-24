79471e9a86ab0af2dc04205a8186dd28961a05bd
vivion <vivonhyt@gmail.com>
2025-06-24 15:15:22 +0800

    新增重写规则以支持API路由，添加echo测试接口并自定义响应格式

--- fake-server/mockServer.js
+++ fake-server/mockServer.js
@@ -3,8 +3,21 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(__dirname + "/db.json");
 const middlewares = jsonServer.defaults();
-
+const rewriter = jsonServer.rewriter({
+  "/api/*": "/$1",
+});
+server.use(rewriter);
 server.use(middlewares);
+server.get("/echo", (req, res) => {
+  res.jsonp({ test: "123" });
+});
+router.render = (req, res) => {
+  res.jsonp({
+    list: res.locals.data,
+    count: res.locals.data.length,
+  });
+};
+
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
