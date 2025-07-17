2c2655543e4f1818368940d9d39716128f3de6b2
vivion <vivonhyt@gmail.com>
2025-07-17 20:33:03 +0800

    新增.env.analyze文件以配置生产环境和分析模式；安装cross-env依赖适应不同操作系统

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,20 @@
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const { defineConfig } = require("@vue/cli-service");
+const isAnalyzeMode = !!process.env.ANALYZE_MODE;
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.(tsx|jsx)$/,
-          use: ["babel-loader"],
-        },
-      ],
-    },
-    plugins: [new BundleAnalyzerPlugin()],
+  configureWebpack: (config) => {
+    // 配置 JSX/TSX 支持
+    config.module.rules.push({
+      test: /\.(tsx|jsx)$/,
+      use: ["babel-loader"],
+    });
+
+    // 只在分析模式下添加 BundleAnalyzerPlugin
+    if (isAnalyzeMode) {
+      config.plugins.push(new BundleAnalyzerPlugin());
+    }
   },
 });
